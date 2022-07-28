879cce0659c3803ff32f120197c74a76a74632ce
jtardioli <joshua.tardioli@gmail.com>
2022-07-28 17:49:18 -0400

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <meta
+          name="Amazon Clone"
+          content="Full Stack amazon clone with web2 & web3 checkout"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Hi</h1>
+    </div>
+  );
+};
+
+export default Home;
